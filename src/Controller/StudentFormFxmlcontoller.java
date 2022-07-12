0ee7d9f2cc0b5aefe9b778268ef07badee3214b0
Himasha piumanthi <101160348+Himababii@users.noreply.github.com>
2022-07-12 11:33:57 +0530

    firstupload

---
+++ src/Controller/StudentFormFxmlcontoller.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class StudentFormFxmlcontoller {
+    public JFXTextField studentid;
+    public JFXTextField nameid;
+    public JFXTextField mailid;
+    public JFXTextField contactid;
+    public JFXTextField addressid;
+    public JFXTextField nicid;
+
+    public void addstudentonaction(ActionEvent actionEvent) {
+    }
+
+    public void updatestudentonaction(ActionEvent actionEvent) {
+    }
+
+    public void deletestudenonaction(ActionEvent actionEvent) {
+    }
+
+    public void SearchOnaction(ActionEvent actionEvent) {
+    }
+}
